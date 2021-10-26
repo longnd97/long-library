c980b89c9cd0525ca83e59c8eb339026dd463882
longnd97 <nduylong060697@gmail.com>
2021-10-26 13:01:37 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LoginController::class, 'showFormLogin']);
 Route::get('/register', [RegisterController::class, 'showFormRegister'])->name('register');
 Route::post('/register', [RegisterController::class, 'register'])->name('auth.register');
-Route::get('/login', [LoginController::class, 'showFormLogin'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('auth.login');
 Route::get('/auth/redirect/{provider}', [SocialController::class, 'redirect']);
 Route::get('{provider}/callback/', [SocialController::class, 'callback']);
